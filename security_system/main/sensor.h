183734aaaadb39c2996dd19ac5c803b2e5e61cd6
mitazet <mitazet@gmail.com>
2017-10-22 11:24:26 +0900

    modify header file

--- security_system/main/sensor.h
+++ security_system/main/sensor.h
@@ -1,5 +1,15 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
 #include "driver/i2c.h"
 
+// Software Reset 
 extern esp_err_t reset_sensor();
+
+// Init i2c communication
 extern void init_sensor();
+
+// Read distancee from sensor
 extern esp_err_t read_distance(int *distance);
+
+#endif
